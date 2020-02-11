cf46d6a802d49b6359ed68b36a50875bc26e3f27
Benjamin Teuber <bsteuber@gmail.com>
2020-02-11 13:57:11 +0100

    Add host

--- js/sensei_env.js
+++ js/sensei_env.js
@@ -1,4 +1,5 @@
 SENSEI_ENV = {
   FIREBASE_DB: "PRODUCTION",
-  PATH_PREFIX: "ai-sensei-beta"
+  PATH_PREFIX: "ai-sensei-beta",
+  SERVER_HOST: "https://us-central1-sensei-160117.cloudfunctions.net/prodServer"
 };
