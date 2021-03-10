c13e2462f48330ef70f1fd85112f95eadd7773a4
Benjamin Teuber <bsteuber@gmail.com>
2021-03-10 21:38:05 +0100

    Config for serving under /beta

--- js/sensei_env.js
+++ js/sensei_env.js
@@ -1,6 +1,6 @@
 SENSEI_ENV = {
   FIREBASE_DB: "PRODUCTION",
-  PATH_PREFIX: "ai-sensei-beta",
+  PATH_PREFIX: "beta",
   SERVER_HOST: "https://us-central1-sensei-160117.cloudfunctions.net/betaServer",
   WS_SCHEDULER_URL: "wss://weiqi-ai.com",
   ENABLE_LIVE_MODE: "true",
