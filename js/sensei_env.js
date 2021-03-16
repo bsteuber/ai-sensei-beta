a7221c0cf21e5d9d8b9f71d95f58d912b8e44e4e
Benjamin Teuber <bsteuber@gmail.com>
2021-03-16 12:48:11 +0100

    Remove site, add moved notification

--- js/sensei_env.js
+++
@@ -1,8 +0,0 @@
-SENSEI_ENV = {
-  FIREBASE_DB: "PRODUCTION",
-  PATH_PREFIX: "ai-sensei-beta",
-  SERVER_HOST: "https://us-central1-sensei-160117.cloudfunctions.net/betaServer",
-  WS_SCHEDULER_URL: "wss://weiqi-ai.com/ws",
-  ENABLE_LIVE_MODE: "true",
-  ENABLE_FAST_LEVEL_UP: "true"
-};
