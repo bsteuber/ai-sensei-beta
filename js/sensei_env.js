2b4adaaf3562c688224e13ceffc0ac82ca847d6d
Benjamin Teuber <bsteuber@gmail.com>
2021-03-30 09:29:34 +0200

    Update ws-scheduler-url to wss://ai-sensei.com/ws

--- js/sensei_env.js
+++ js/sensei_env.js
@@ -2,7 +2,7 @@ SENSEI_ENV = {
   FIREBASE_DB: "PRODUCTION",
   PATH_PREFIX: "beta",
   SERVER_HOST: "https://us-central1-sensei-160117.cloudfunctions.net/betaServer",
-  WS_SCHEDULER_URL: "wss://weiqi-ai.com/ws",
+  WS_SCHEDULER_URL: "wss://ai-sensei.com/ws",
   ENABLE_LIVE_MODE: "true",
   ENABLE_FAST_LEVEL_UP: "true"
 };
