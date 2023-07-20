51d46f4447ac641da830d7d2dcdcd6fe69f46356
Benjamin Teuber <bsteuber@gmail.com>
2023-07-20 15:32:07 +0200

    "Update AI-Sensei"

--- js/sensei_env.js
+++ js/sensei_env.js
@@ -3,6 +3,7 @@ SENSEI_ENV = {
   FIREBASE_DB: "PRODUCTION",
   SERVER_HOST: "https://us-central1-sensei-160117.cloudfunctions.net/betaServer",
   RANK_PREDICT_HOST: "https://ai-sensei.com/rank-predict",
+  HUMANLIKE_HOST: "https://ai-sensei.com/predict-humanlike",
   WS_SCHEDULER_URL: "wss://ai-sensei.com/ws",
   ENABLE_HUMANLIKE_BOTS: "true",
   ENABLE_FAST_LEVEL_UP: "true",
